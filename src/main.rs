8b6c6473e76dd51b6fb0fd3873872cbb8097aac6
liufeihua <1002219331@qq.com>
2025-03-12 10:34:26 +0800

    feat(config): implement configuration file loading and parsing
    - Add support for loading and parsing a configuration file in TOML format
    - Introduce new configuration structures for server and database settings
    - Update the main function to use the parsed configuration- Refactor the route definition into a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #[macro_use]
 extern crate rbatis;
 
+use crate::middleware::auth::auth_token;
+use crate::routes::{build_other_route, build_system_route};
+use config::{Config, File};
+use handler::system::sys_user_handler::*;
 use once_cell::sync::Lazy;
 use rbatis::RBatis;
 use salvo::prelude::*;
-
-use crate::middleware::auth::auth_token;
-use crate::routes::{build_other_route, build_system_route};
-use handler::system::sys_user_handler::*;
+use serde::Deserialize;
 
 pub mod common;
 pub mod handler;
@@ -23,21 +24,58 @@ pub static RB: Lazy<RBatis> = Lazy::new(RBatis::new);
 async fn hello() -> &'static str {
     "Hello World123123"
 }
+// 定义应用状态结构体，包含数据库连接池
+pub struct AppState {
+    pub batis: RBatis,
+}
 
+// 定义配置结构体，包含服务器和数据库配置
+#[derive(Debug, Deserialize)]
+struct Config1 {
+    server: ServerConfig,
+    db: DbConfig,
+}
+
+// 定义服务器配置结构体
+#[derive(Debug, Deserialize)]
+struct ServerConfig {
+    addr: String,
+}
+
+// 定义数据库配置结构体
+#[derive(Debug, Deserialize)]
+struct DbConfig {
+    url: String,
+}
+
+// 主函数，异步运行
 #[tokio::main]
 async fn main() {
+    // 初始化日志系统
     log4rs::init_file("src/config/log4rs.yaml", Default::default()).unwrap();
     // tracing_subscriber::fmt().init();
+    
+    // 加载和解析配置文件
+    let config = Config::builder()
+        .add_source(File::with_name("config.toml"))
+        .build()
+        .unwrap()
+        .try_deserialize::<Config1>()
+        .unwrap();
+    println!("Config: {:?}", config);
 
-    let mysql_url = "mysql://root:123456@127.0.0.1:3306/rustdb";
-    RB.init(rbdc_mysql::driver::MysqlDriver {}, mysql_url)
+    // 初始化数据库连接
+    RB.init(rbdc_mysql::driver::MysqlDriver {}, config.db.url.as_str())
         .unwrap();
 
-    let acceptor = TcpListener::new("0.0.0.0:8100").bind().await;
+    // 创建TCP监听器并启动服务器
+    let acceptor = TcpListener::new(config.server.addr).bind().await;
     Server::new(acceptor).serve(route()).await;
 }
 
+// 定义路由配置函数
 fn route() -> Router {
+    // 创建路由实例，配置API路径和处理函数
     Router::new()
         .path("/api")
         .get(hello)
